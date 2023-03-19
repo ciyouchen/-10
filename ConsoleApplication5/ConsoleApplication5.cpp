71158bea3312348567a957dbec937de5c777110a
User <User@TNACLT28079>
2023-03-19 13:22:42 +0800

    加入專案檔案。

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+using namespace std;
+
+int euc(int a, int b);//輾轉相除法遞迴函式宣告 
+int main()
+{
+	int in = 0, in2 = 0, c = 0;
+
+	cin >> in >> in2;
+	if (in < in2)//確認大小順序 
+	{
+		c = in;
+		in = in2;
+		in2 = c;
+	}
+	cout << euc(in, in2) << endl;
+	return 0;
+}
+int euc(int a, int b)
+{
+	int f = a % b;
+	if (f == 0) return b;
+	else return euc(b, f);
+}
